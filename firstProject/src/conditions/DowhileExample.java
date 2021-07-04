bca12d49b4692ea5c410c028a31a54991cbb5e01
sec <sec@192.168.219.101>
2021-07-04 18:19:41 +0900

    변경사항

--- firstProject/src/conditions/DowhileExample.java
+++ firstProject/src/conditions/DowhileExample.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class DowhileExample {
 	public static void main(String[] args) {
+		System.out.println("이름을 입력하세요.");
+		System.out.println("종료하시려면 q를 입력하세요.");
+		
 		Scanner scn = new Scanner(System.in);
+		String name;
+		
 		do {
-			System.out.println("이름을 입력: ");
-			String name = scn.nextLine();
-			if (name.equals("quit")) {
-				break;
-			}
+			System.out.print("이름: ");
+			name = scn.nextLine();
 			System.out.println("입력이름: " + name);
-		} while (false);
+			
+			
+			} while (!name.equals("q"));
+			
+		System.out.println();
 		System.out.println("프로그램 종료.");
+		
 	}
 
 }
